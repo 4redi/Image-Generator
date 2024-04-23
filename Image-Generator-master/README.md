ecff135fde1298400c0cc76c5d52421672ccaf9a
wolf123t <redicurrii@yahoo.com>
2024-04-23 18:53:44 +0200

    added files

---
+++ Image-Generator-master/README.md
@@ -0,0 +1,48 @@
+# Installation
+To use the Image generator locally, follow these steps:
+
+Clone the repository: `git clone https://github.com/wolf123t/Image-Generator.git`
+
+Open the `image_generator.py` file
+
+
+# Image-Generator
+Experience the simplicity of creating captivating images with our Python-based Image Generator powered by the OpenAI API. This user-friendly app is designed for quick and hassle-free image generation. With a straightforward interface, it offers prompt-based image creation, style selection, and the ability to generate multiple images effortlessly.
+# Key features
+* Prompt-Based Creativity:
+  * Begin by entering a brief textual description or concept for your image. The AI will use this prompt to generate a unique visual representation.
+    
+* Style Selection:
+  * Choose from a set of predefined artistic styles or create your own. Apply these styles to your prompt to give your images a distinct look and feel.
+    
+* Image Quantity Control:
+  * Customize the number of images you want to generate. You have control over the output, whether it's one image or a batch of them.
+    
+* Swift AI-Powered Results:
+  * Our application leverages the OpenAI API's cutting-edge machine-learning models to transform your prompts into visually appealing images swiftly.
+    
+* Minimal Learning Curve:
+  * This app is designed with simplicity in mind, making it accessible to users of all skill levels. You don't need to be a Python expert to create stunning images.
+
+# Preview
+This is what the program looks like, It's simple to use and very user-friendly.
+
+<div>
+ <img src="images/pic_img_gen.png" alt="example">
+</div>
+
+# An example
+Here in this example, I'm using one image, a realistic style, and a simple prompt. And It's generated. If you ask for more than 1 image (10 maximum), you'll get a slideshow of all of them.
+
+<div>
+ <img src="images/exampe_of_imggen.png" alt="image2">
+</div>
+
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+ 
+## THINGS TO KNOW
+- Add your own API key, so the programm can work
---
+++ Image-Generator-master/image_generator/image_generator.py
@@ -0,0 +1,62 @@
+import customtkinter as ctk
+import tkinter as tk
+import os 
+import openai
+from PIL import Image,ImageTk
+import requests,io
+def generate_image():
+    openai.api_key=os.getenv('OPENAI_API_KEY')
+    user_prompt=prompt_entry.get("0.0",tk.END)
+    user_prompt+="in style: "+style_dropdown.get()
+    response=openai.Image.create(
+    prompt=user_prompt,
+    n=int(number_range.get()),
+    size="512x512"
+)
+    image_url=[]
+    for i in range(len(response['data'])):
+        image_url.append(response['data'][i]['url'])
+    print(image_url)
+
+    images=[]
+    for url in image_url:
+        response=requests.get(url)
+        image=Image.open(io.BytesIO(response.content))
+        photo_image=ImageTk.PhotoImage(image)
+        images.append(photo_image)
+
+    def update_photo_image(index=0):
+        canvas.image=images[index]
+        canvas.create_image(0,0,anchor="nw",image=images[index])
+        index=(index+1) % len(images)
+        canvas.after(3000,update_photo_image,index)
+    update_photo_image()        
+root=ctk.CTk()
+# * window
+root.title("Image Generator Project")
+ctk.set_appearance_mode("dark")
+# * The box that holds the values
+input_frame=ctk.CTkFrame(root)
+input_frame.pack(side="left",expand=True,padx=20,pady=20)
+#*promt value and its text box
+prompt_label=ctk.CTkLabel(input_frame,text="Prompt")
+prompt_label.grid(row=0,column=0,padx=10,pady=10)
+prompt_entry=ctk.CTkTextbox(input_frame,height=10)
+prompt_entry.grid(row=0,column=1,padx=10,pady=10)
+#*style label and its dropdown
+style_label=ctk.CTkLabel(input_frame,text="Style")
+style_label.grid(row=1,column=0,padx=10,pady=10)
+style_dropdown=ctk.CTkComboBox(input_frame,values=["Realistic","Cartoon","3D","Flat Art"])
+style_dropdown.grid(row=1,column=1,padx=10,pady=10)
+#*number of images and its range
+number_label=ctk.CTkLabel(input_frame,text="Img no.")
+number_label.grid(row=2,column=0,padx=10,pady=10)
+number_range=ctk.CTkSlider(input_frame,from_=1,to=10,number_of_steps=9)
+number_range.grid(row=2,column=1)
+#*generate button
+button_generate=ctk.CTkButton(input_frame,text="Generate",command=generate_image)
+button_generate.grid(row=3,column=0,columnspan=2,sticky="news",padx=10,pady=10)
+#* canvas
+canvas=tk.Canvas(root,width=512,height=512)
+canvas.pack(side="left")
+root.mainloop()
